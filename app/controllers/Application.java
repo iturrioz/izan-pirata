26b017ba716ce10542ca4d26b10f6d8a73e8d215
Kepa <dittbi@gmail.com>
2014-07-26 01:13:26 +0200

    route berriak

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -21,14 +21,14 @@ public class Application extends Controller {
         return ok(index.render());
     }
 
-    public static Result addPerson() {
-    	Person person = Form.form(Person.class).bindFromRequest().get();
-    	person.save();
-    	return redirect(routes.Application.index());
-    }
+//    public static Result addPerson() {
+//    	Person person = Form.form(Person.class).bindFromRequest().get();
+//    	person.save();
+//    	return redirect(routes.Application.index());
+//    }
 
-    public static Result getPersons() {
-    	List<Person> persons = new Model.Finder(String.class, Person.class).all();
-    	return ok(toJson(persons));
+    public static Result bomb(String partidaid, String userid, int column, int row) {
+        // TODO deitu Igorren funtzioa
+        return redirect(routes.Application.index());
     }
 }
